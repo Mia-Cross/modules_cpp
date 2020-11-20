8ed56985c3bbcf09b4342e13ac18cbe55627acf4
Mia-Cross <leila.marabese@gmail.com>
2020-11-20 05:38:42 +0100

    baseline for ex01

--- cpp00/ex01/annuaire.cpp
+++ cpp00/ex01/annuaire.cpp
@@ -6,24 +6,27 @@ int main (int ac, char **av)
     int i;
 
     std::cout << "WHAT DO YOU WANT TO DO ? (ADD, SEARCH, EXIT)" << std::endl;
-    getline (std::cin, input);
-    if (input.compare("EXIT") == 0)
-        return 0;
-    else if (input.compare("ADD") == 0)
+    while (getline (std::cin, input) && input.compare("EXIT") != 0)
     {
-        //CREATE CLASS ELEMENT (MALLOC)
-        std::cout << "OK, PLEASE ENTER -FIRST NAME- :" << std::endl;
-        getline (std::cin, input);
-        
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-        return 0;
+        if (input.compare("ADD") == 0)
+        {
+            //CREATE CLASS ELEMENT (MALLOC)
+            std::cout << "OK, PLEASE ENTER -FIRST NAME- :" << std::endl;
+            getline (std::cin, input);
+
+            std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+            return 0;
+        }
+        else if (input.compare("SEARCH") == 0)
+        {
+            //methode
+        }
+        else
+        {
+            std::cout << "UNKNOWN INSTRUCTION" << std::endl;
+            std::cout << "WHAT DO YOU WANT TO DO ? (ADD, SEARCH, EXIT)" << std::endl;
+        }
     }
-    for (i = 1; i < ac; i++)
-    {
-        input = av[i];
-        for (char c : input)
-            putchar(toupper(c));
-    }
-    std::cout << std::endl;
+    std::cout << "OK, BYE !" << std::endl;
     return 0;
 }
