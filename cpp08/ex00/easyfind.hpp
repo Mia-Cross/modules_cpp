5275b0cfde5e4ee0e111fbd5dcdffb051aa8b631
Mia-Cross <leila.marabese@gmail.com>
2021-01-24 21:05:11 +0100

    08/ex00 : added tests with vect, list, set & deque

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: lemarabe <lemarabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 03:34:22 by lemarabe          #+#    #+#             */
-/*   Updated: 2021/01/21 05:39:48 by lemarabe         ###   ########.fr       */
+/*   Updated: 2021/01/24 20:49:06 by lemarabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,12 @@ class CantFindException : public std::exception
 template < typename T >
 typename T::const_iterator easyfind(T const &container, int to_find){
     typename T::const_iterator it;
-    // typename T::const_iterator ite = container.end();
-    // for (it = container.begin(); it != ite ; it++) {
-    //     if (*it == to_find)
-    //         return (it);
-    // }
-    it = container.find(to_find);
+    typename T::const_iterator ite = container.end();
+    for (it = container.begin(); it != ite ; it++) {
+        if (*it == to_find)
+            return (it);
+    }
+   // it = container.find(to_find);
     throw CantFindException();
 }
 
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,25 +6,35 @@
 /*   By: lemarabe <lemarabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 03:34:24 by lemarabe          #+#    #+#             */
-/*   Updated: 2021/01/21 05:44:49 by lemarabe         ###   ########.fr       */
+/*   Updated: 2021/01/24 21:03:16 by lemarabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 #include <vector>
-#include <map>
+#include <set>
+#include <list>
+#include <deque>
 
 int main()
 {
     std::cout << ">> INT VECTOR\n";
     std::vector<int> intVector;
     for (int i = 1; i < 13; i++) { intVector.push_back(i * 10); };
+    try {
     std::cout << *(::easyfind(intVector, 100)) << std::endl;
     std::cout << *(::easyfind(intVector, 42)) << std::endl;
+    } catch (std::exception &e) {
+        std::cout << e.what();
+    }
     
     std::cout << ">> INT VECTOR CONST\n";
     std::vector<int> const constVector(12, 42);
+    try {
     std::cout << *(::easyfind(constVector, 42)) << std::endl;
+    } catch (std::exception &e) {
+        std::cout << e.what();
+    }
 
     std::cout << ">> CHAR VECTOR\n";
     std::vector<char> charVector;
@@ -33,14 +43,39 @@ int main()
     charVector.push_back('a');
     charVector.push_back('r');
     charVector.push_back('*');
+    try {
     std::cout << *(::easyfind(charVector, 42)) << std::endl;
+    } catch (std::exception &e) {
+        std::cout << e.what();
+    }
 
-    std::cout << ">> INT MAP (an int paired with an other variable\n";
-    std::map<int, std::string>  intMap;
-    std::pair<int, std::string> result;
-    intMap[3] = "first number";
-    intMap[78] = "second number";
-    intMap[-966] = "third number";
-    result = *(::easyfind(intMap, -966));
-    std::cout << "Found " << result.second << ", paired with " << result.first << std::endl;
+    std::cout << ">> INT SET\n";
+    std::set<int> intSet;
+    for (int i = 1; i < 5; i++) { intSet.insert(-i); };
+    try {
+    std::cout << *(::easyfind(intSet, -4)) << std::endl;
+    std::cout << *(::easyfind(intSet, -12)) << std::endl;
+    } catch (std::exception &e) {
+        std::cout << e.what();
+    }
+        
+    std::cout << ">> INT DOUBLE-ENDED QUEUE\n";
+    std::deque<int> intDeque;
+    for (int i = 1; i < 5; i++) { intDeque.push_back(i + 1); };
+    try {
+    std::cout << *(::easyfind(intDeque, 5)) << std::endl;
+    std::cout << *(::easyfind(intDeque, 0)) << std::endl;
+    } catch (std::exception &e) {
+        std::cout << e.what();
+    }
+
+    std::cout << ">> INT LIST\n";
+    std::list<int> intList;
+    for (int i = 1; i < 5; i++) { intList.push_back(i + 1); };
+    try {
+    std::cout << *(::easyfind(intList, 5)) << std::endl;
+    std::cout << *(::easyfind(intList, 0)) << std::endl;
+    } catch (std::exception &e) {
+        std::cout << e.what();
+    }
 }
